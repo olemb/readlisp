a21abf6dad8595e17429926ddecbb1b0f9c25317
Ole Martin Bjørndalen <ombdalen@gmail.com>
2020-11-25 17:22:46 +0100

    Updated tests.

--- test_readlisp.py
+++ test_readlisp.py
@@ -4,3 +4,8 @@ from readlisp import readlisp, LispSymbol
 assert readlisp('(a |b|)') == [LispSymbol('a'), LispSymbol('b')]
 
 assert readlisp('(1 2 3 "four")') == [1, 2, 3, "four"]
+
+# TODO: How do we handle these cases? (And what does this mean?)
+# assert readlisp("(a '|b|)") == ?
+# assert readlisp("(a :|b|)") == ?
+
